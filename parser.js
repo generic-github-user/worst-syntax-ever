e61efa9b099b28116502cde2958876f159870d25
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-07-07 11:42:47 -0400

    Added use-symbols and use-spaces

---
+++ parser.js
@@ -0,0 +1,26 @@
+console.log("Worst Syntax Ever loaded.");
+var page = document.body.innerHTML;
+
+const parseSymbols = function () {
+	console.log("Symbol syntax enabled.");
+}
+const parseSpaces = function () {
+	console.log("Space syntax enabled.");
+}
+
+const u_sym = page.indexOf("%use-symbols%");
+const u_spa = page.indexOf("%use-spaces%");
+if (u_sym !== -1) {
+	parseSymbols();
+}
+else if (u_sym !== -1) {
+	parseSpaces();
+}
+else {
+	parseSymbols();
+}
+
+page = page.replace(/%use-symbols%/g, "");
+page = page.replace(/%use-spaces%/g, "");
+
+document.body.innerHTML = page;
