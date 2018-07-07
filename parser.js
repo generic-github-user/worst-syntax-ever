a3801aee25430545e85a631582e9ff0678918a67
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-07-07 12:16:52 -0400

    Added opening and closing symbols and JSON symbol lookup table

--- parser.js
+++ parser.js
@@ -1,11 +1,34 @@
 console.log("Worst Syntax Ever loaded.");
 var page = document.body.innerHTML;
 
+const wse = {
+	"main": {
+		"opener": "~[`$*`'|{",
+		"closer": "}/`'$*']~~"
+	},
+	"symbols": {
+		"italic": {
+			"opening": "_-#(",
+			"closing": ")-_#"
+		}
+	}
+}
+const wse_c = [
+	wse.main.opener,
+	wse.main.closer,
+	wse.symbols.italic.opening,
+	wse.symbols.italic.closing
+];
+for (var i = 2; i < wse_c.length; i += 2) {
+	wse_c[i] = wse.main.opener + wse_c[i];
+	wse_c[i + 1] += wse.main.closer;
+}
+
 const parseSymbols = function () {
 	console.log("Symbol syntax enabled.");
 
-	page = page.replace("-#(", "<span style='font-style: italic;'>");
-	page = page.replace(")-#", "<span style='font-style: italic;'>");
+	page = page.replace(wse_c[2], "<span style='font-style: italic;'>");
+	page = page.replace(wse_c[3], "<span style='font-style: italic;'>");
 }
 const parseSpaces = function () {
 	console.log("Space syntax enabled.");
