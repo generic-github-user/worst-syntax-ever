dd3223391ab3f08e08f1422290a0b98157cf99e3
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-07-10 12:37:46 -0400

    Added headers (#16)

--- syntax.js
+++ syntax.js
@@ -42,6 +42,72 @@ const wse = {
 						"syntax": ")-_$",
 						"result": "</span>"
 					}
+				},
+				{
+					"name": "header_1",
+					"opening": {
+						"syntax": "=1/^[",
+						"result": "<h1>"
+					},
+					"closing": {
+						"syntax": "]^=1//",
+						"result": "</h1>"
+					}
+				},
+				{
+					"name": "header_2",
+					"opening": {
+						"syntax": "=2/^^[",
+						"result": "<h2>"
+					},
+					"closing": {
+						"syntax": "]^^=2//",
+						"result": "</h2>"
+					}
+				},
+				{
+					"name": "header_3",
+					"opening": {
+						"syntax": "=3/^^^[",
+						"result": "<h3>"
+					},
+					"closing": {
+						"syntax": "]^^^=3//",
+						"result": "</h3>"
+					}
+				},
+				{
+					"name": "header_4",
+					"opening": {
+						"syntax": "=4/^^^^[",
+						"result": "<h4>"
+					},
+					"closing": {
+						"syntax": "]^^^^=4//",
+						"result": "</h4>"
+					}
+				},
+				{
+					"name": "header_5",
+					"opening": {
+						"syntax": "=5/^^^^^[",
+						"result": "<h5>"
+					},
+					"closing": {
+						"syntax": "]^^^^^=5//",
+						"result": "</h5>"
+					}
+				},
+				{
+					"name": "header_6",
+					"opening": {
+						"syntax": "=6/^^^^^^[",
+						"result": "<h6>"
+					},
+					"closing": {
+						"syntax": "]^^^^^^=6//",
+						"result": "</h6>"
+					}
 				}
 			],
 			"full": [
